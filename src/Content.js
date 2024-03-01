964d276dad17d42464efe0d37a5ce5c05449f243
VRUSHABH SOMANI <vssomani2002@gmail.com>
2024-03-01 22:56:18 +0530

    comments removed

--- src/Content.js
+++ src/Content.js
@@ -1,17 +1,7 @@
 import { useState } from "react";
 import { FaTrash } from "react-icons/fa";
 const Content = () => {
-  //Random Name
-  // const [name, setName] = useState('User');
-  // const nameChange =()=>{
-  //   const names = ['VS','DS','RS','HS','PS','JS'];
-  //   const res = Math.floor(Math.random() * names.length);
-  //   setName(names[res]);
-  // }
-  
-  // const btnClick=()=>{
-  //   console.log("Button clicked!");
-  // }
+
   const  [items ,setItems]=useState([
     {
       id: 1,
@@ -33,13 +23,13 @@ const Content = () => {
   const handleChange =(id) =>{
     const listItems = items.map((i) => i.id === id ? {...i,checked : !i.checked} : i);
     setItems(listItems);
-    localStorage.setItems('To-Do List', JSON.stringify(listItems));
+    localStorage.setItem('To-Do List', JSON.stringify(listItems));
   }
 
   const handleDelete =(id) =>{
     const removeItems = items.filter((i)=> i.id !== id );
     setItems(removeItems);
-    localStorage.setItems('To-Do List', JSON.stringify(removeItems));
+    localStorage.setItem('To-Do List', JSON.stringify(removeItems));
   }
   return (
     <main>
